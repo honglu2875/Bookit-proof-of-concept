4c75436b0e90407e0cc5c89a6f87ae45f63d589a
Void <honglu2875@gmail.com>
2022-06-03 19:34:11 -0400

    fix

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+help()
+{
+	echo "Please first supply the following environment variables:"
+	echo
+	echo "CLIENT_ID: Google API client id."
+	echo "CLIENT_SECRET: Google API client secret."
+	echo "POSTGRES_USERNAME: the username for postgres database."
+	echo "POSTGRES_PASSWORD: the password for postgres database."
+	echo ""
+}
