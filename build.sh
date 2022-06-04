7da09e9efa46af03c80ebd75f360a2f027e71ebe
Void <honglu2875@gmail.com>
2022-06-04 19:35:33 -0400

    update submodules

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ help()
 	echo "CLIENT_ID: Google API client id."
 	echo "CLIENT_SECRET: Google API client secret."
 	echo "API_KEY: Google API key."
+	echo "POSTGRES_ADDR: (optional, default 'db') the address or name of the postgres server"
 	echo "POSTGRES_USER: (optional, default 'postgres') the username for postgres database."
 	echo "POSTGRES_PASSWORD: (optional, default 'password') the password for postgres database."
 	echo "EMAIL: the main host's email address."
@@ -15,6 +16,10 @@ if [[ -z "${CLIENT_ID}" ]] || [[ -z "${CLIENT_SECRET}" ]] || [[ -z "${API_KEY}" 
 	help
 	exit 0
 fi	
+
+if [[ -z "${POSTGRES_ADDR}" ]]; then
+	POSTGRES_ADDR="db"
+fi
 
 if [[ -z "${POSTGRES_USER}" ]]; then
 	POSTGRES_USER="postgres"
@@ -31,6 +36,7 @@ sed -i "s/<your client id>/$CLIENT_ID/g" $DBMANAGER_RESOURCES_PATH/application.y
 sed -i "s/<your client secret>/$CLIENT_SECRET/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<Postgres username>/$POSTGRES_USER/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<Postgres password>/$POSTGRES_PASSWORD/g" $DBMANAGER_RESOURCES_PATH/application.yml
+sed -i "s/<Postgres addr>/$POSTGRES_ADDR/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<your google api key>/$API_KEY/g" $DBMANAGER_RESOURCES_PATH/application.yml
 
 [ ! -e .env ] || rm .env
@@ -43,6 +49,4 @@ echo "EMAIL=$EMAIL" >> .env
 
 cd Bookit-dbManager
 ./build.sh
-cd ..
-
 
