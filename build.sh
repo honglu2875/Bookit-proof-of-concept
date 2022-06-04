6c9c121e7616c299172b39fdd5d4da55f4985bf2
Void <honglu2875@gmail.com>
2022-06-04 18:51:28 -0400

    update docker-compose.yml

--- build.sh
+++ build.sh
@@ -6,18 +6,18 @@ help()
 	echo "CLIENT_ID: Google API client id."
 	echo "CLIENT_SECRET: Google API client secret."
 	echo "API_KEY: Google API key."
-	echo "POSTGRES_USERNAME: (optional, default 'postgres') the username for postgres database."
+	echo "POSTGRES_USER: (optional, default 'postgres') the username for postgres database."
 	echo "POSTGRES_PASSWORD: (optional, default 'password') the password for postgres database."
-	echo 
+	echo "EMAIL: the main host's email address."
 }
 
-if [[ -z "${CLIENT_ID}" ]] || [[ -z "${CLIENT_SECRET}" ]] || [[ -z "${API_KEY}" ]]; then
+if [[ -z "${CLIENT_ID}" ]] || [[ -z "${CLIENT_SECRET}" ]] || [[ -z "${API_KEY}" ]] || [[ -z "${EMAIL}" ]]; then
 	help
 	exit 0
 fi	
 
-if [[ -z "${POSTGRES_USERNAME}" ]]; then
-	POSTGRES_USERNAME="postgres"
+if [[ -z "${POSTGRES_USER}" ]]; then
+	POSTGRES_USER="postgres"
 fi
 
 if [[ -z "${POSTGRES_PASSWORD}" ]]; then
@@ -29,10 +29,20 @@ DBMANAGER_RESOURCES_PATH="Bookit-dbManager/src/main/resources"
 cp $DBMANAGER_RESOURCES_PATH/application.yml.example $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<your client id>/$CLIENT_ID/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<your client secret>/$CLIENT_SECRET/g" $DBMANAGER_RESOURCES_PATH/application.yml
-sed -i "s/<Postgres username>/$POSTGRES_USERNAME/g" $DBMANAGER_RESOURCES_PATH/application.yml
+sed -i "s/<Postgres username>/$POSTGRES_USER/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<Postgres password>/$POSTGRES_PASSWORD/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<your google api key>/$API_KEY/g" $DBMANAGER_RESOURCES_PATH/application.yml
+
+[ ! -e .env ] || rm .env
+echo "CLIENT_ID=$CLIENT_ID" >> .env
+echo "CLIENT_SECRET=$CLIENT_SECRET" >> .env
+echo "POSTGRES_USER=$POSTGRES_USER" >> .env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" >> .env
+echo "API_KEY=$API_KEY" >> .env
+echo "EMAIL=$EMAIL" >> .env
 
 cd Bookit-dbManager
 ./build.sh
 cd ..
+
+
