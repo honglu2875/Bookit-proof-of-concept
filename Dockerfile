2a280c8aef8df01adc11b2b922b5b94541c391cb
Void <honglu2875@gmail.com>
2022-06-05 07:09:22 -0400

    update build script

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17
-COPY dbManager /usr/src/app
-WORKDIR /usr/src/app
-RUN java -jar dbManager
--- build.sh
+++ build.sh
@@ -6,13 +6,14 @@ help()
 	echo "CLIENT_ID: Google API client id."
 	echo "CLIENT_SECRET: Google API client secret."
 	echo "API_KEY: Google API key."
+	echo "EMAIL: the main host's email address."
+	echo "REDIRECT_URI: the uri for redirection from google OAuth2"
 	echo "POSTGRES_ADDR: (optional, default 'db') the address or name of the postgres server"
 	echo "POSTGRES_USER: (optional, default 'postgres') the username for postgres database."
 	echo "POSTGRES_PASSWORD: (optional, default 'password') the password for postgres database."
-	echo "EMAIL: the main host's email address."
 }
 
-if [[ -z "${CLIENT_ID}" ]] || [[ -z "${CLIENT_SECRET}" ]] || [[ -z "${API_KEY}" ]] || [[ -z "${EMAIL}" ]]; then
+if [[ -z "${CLIENT_ID}" ]] || [[ -z "${CLIENT_SECRET}" ]] || [[ -z "${API_KEY}" ]] || [[ -z "${EMAIL}" ]] || [[ -z "${REDIRECT_URI}" ]]; then
 	help
 	exit 0
 fi	
@@ -34,10 +35,10 @@ DBMANAGER_RESOURCES_PATH="Bookit-dbManager/src/main/resources"
 cp $DBMANAGER_RESOURCES_PATH/application.yml.example $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<your client id>/$CLIENT_ID/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<your client secret>/$CLIENT_SECRET/g" $DBMANAGER_RESOURCES_PATH/application.yml
+sed -i "s/<your google api key>/$API_KEY/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<Postgres username>/$POSTGRES_USER/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<Postgres password>/$POSTGRES_PASSWORD/g" $DBMANAGER_RESOURCES_PATH/application.yml
 sed -i "s/<Postgres addr>/$POSTGRES_ADDR/g" $DBMANAGER_RESOURCES_PATH/application.yml
-sed -i "s/<your google api key>/$API_KEY/g" $DBMANAGER_RESOURCES_PATH/application.yml
 
 [ ! -e .env ] || rm .env
 echo "CLIENT_ID=$CLIENT_ID" >> .env
